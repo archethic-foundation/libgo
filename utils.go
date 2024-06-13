09f3e3e44e15c1e2b352eaae8db728655be452bd
Samuel Manzanera <samuelmanzanera@protonmail.com>
2024-06-13 16:22:14 +0200

    Add ParseBigInt/FormatBigInt

--- utils.go
+++ utils.go
@@ -2,6 +2,8 @@ package archethic
 
 import (
 	"encoding/hex"
+	"errors"
+	"math/big"
 	"regexp"
 )
 
@@ -66,4 +68,55 @@ func ToBigInt(number float64) uint64 {
 
 func FromBigInt(number uint64) float64 {
 	return float64(number) / 100000000
+}
+
+func ParseBigInt(number string, formatDecimals uint) (*big.Int, error) {
+	r := regexp.MustCompile(`^([0-9]*)\.?([0-9]*)$`)
+	matches := r.FindAllStringSubmatch(number, -1)
+	if len(matches) == 0 || len(matches[0]) == 0 || len(matches[0][1])+len(matches[0][2]) == 0 {
+		return nil, errors.New("Invalid number")
+	}
+
+	wholeStr := matches[0][1]
+	decimalStr := matches[0][2]
+
+	// Pad out the decimals
+	for len(decimalStr) < int(formatDecimals) {
+		decimalStr += "0000"
+	}
+
+	// Remove extra padding
+	decimalStr = decimalStr[:formatDecimals]
+
+	b, _ := new(big.Int).SetString(wholeStr+decimalStr, 10)
+
+	return b, nil
+}
+
+func FormatBigInt(b *big.Int, formatDecimals uint8) string {
+	strNumber := b.String()
+	if formatDecimals == 0 {
+		return strNumber
+	}
+
+	// Pad out to the whole component (including a whole digit)
+	for len(strNumber) <= int(formatDecimals) {
+		strNumber = "0000" + strNumber
+	}
+
+	// Insert the decimal point
+	index := len(strNumber) - int(formatDecimals)
+	strNumber = strNumber[:index] + "." + strNumber[index:]
+
+	// Trim the whole component (leaving at least one 0)
+	for strNumber[0] == '0' && strNumber[1] != '.' {
+		strNumber = strNumber[1:]
+	}
+
+	// Trim the decimal component (leaving at least one 0)
+	for strNumber[len(strNumber)-1] == '0' && strNumber[len(strNumber)-2] != '.' {
+		strNumber = strNumber[:len(strNumber)-1]
+	}
+
+	return strNumber
 }
--- utils_test.go
+++ utils_test.go
@@ -2,6 +2,7 @@ package archethic
 
 import (
 	"math"
+	"math/big"
 	"reflect"
 	"testing"
 )
@@ -164,6 +165,88 @@ func TestDecodeVarInt(t *testing.T) {
 
 	actual, _ = DecodeVarInt([]byte{6, 1, 0, 0, 0, 0, 0})
 	expected = uint64(math.Pow(2, 40))
+
+	if !reflect.DeepEqual(expected, actual) {
+		t.Errorf("actual %q, expected %q", actual, expected)
+	}
+}
+
+func TestParseBigInt(t *testing.T) {
+
+	var expected *big.Int
+	var actual *big.Int
+
+	expected = big.NewInt(1253450000)
+	actual, _ = ParseBigInt("12.5345", 8)
+
+	if !reflect.DeepEqual(expected, actual) {
+		t.Errorf("actual %q, expected %q", actual, expected)
+	}
+
+	expected = big.NewInt(12534500)
+	actual, _ = ParseBigInt("12.5345", 6)
+
+	if !reflect.DeepEqual(expected, actual) {
+		t.Errorf("actual %q, expected %q", actual, expected)
+	}
+
+	expected = big.NewInt(1201396945692)
+	actual, _ = ParseBigInt("120139.69456927", 7)
+
+	if !reflect.DeepEqual(expected, actual) {
+		t.Errorf("actual %q, expected %q", actual, expected)
+	}
+
+	expected = big.NewInt(9403999999)
+	actual, _ = ParseBigInt("94.03999999999999", 8)
+
+	if !reflect.DeepEqual(expected, actual) {
+		t.Errorf("actual %q, expected %q", actual, expected)
+	}
+
+	expected = big.NewInt(12534500000000)
+	actual, _ = ParseBigInt("125345", 8)
+
+	if !reflect.DeepEqual(expected, actual) {
+		t.Errorf("actual %q, expected %q", actual, expected)
+	}
+}
+
+func TestFormatBigInt(t *testing.T) {
+
+	var expected string
+	var actual string
+
+	actual = FormatBigInt(big.NewInt(1253450000), 8)
+	expected = "12.5345"
+
+	if !reflect.DeepEqual(expected, actual) {
+		t.Errorf("actual %q, expected %q", actual, expected)
+	}
+
+	actual = FormatBigInt(big.NewInt(12534500), 6)
+	expected = "12.5345"
+
+	if !reflect.DeepEqual(expected, actual) {
+		t.Errorf("actual %q, expected %q", actual, expected)
+	}
+
+	expected = FormatBigInt(big.NewInt(1201396945692), 7)
+	actual = "120139.6945692"
+
+	if !reflect.DeepEqual(expected, actual) {
+		t.Errorf("actual %q, expected %q", actual, expected)
+	}
+
+	expected = FormatBigInt(big.NewInt(9403999999), 8)
+	actual = "94.03999999"
+
+	if !reflect.DeepEqual(expected, actual) {
+		t.Errorf("actual %q, expected %q", actual, expected)
+	}
+
+	expected = FormatBigInt(big.NewInt(12534500000000), 8)
+	actual = "125345.0"
 
 	if !reflect.DeepEqual(expected, actual) {
 		t.Errorf("actual %q, expected %q", actual, expected)
